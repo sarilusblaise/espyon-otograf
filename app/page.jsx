cf1e5ed68c5888d3e5259da555e206ef7acd72ee
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-07-11 09:59:11 -0400

    added paragraph description to home page and new styles

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,17 @@ import Header from '../components/Header';
 export default function HomePage() {
 	return (
 		<main>
-			<h2 className='mx-auto max-w-4xl font-display text-5xl text-center font-bold tracking-normal text-gray-400 sm:text-7xl '>
-				<span>Espyon</span> <span>Ortograf</span> le correcteur approprie
-			</h2>
+			<div>
+				<h2 className='mx-auto max-w-4xl font-display text-5xl text-center font-bold tracking-normal text-white sm:text-7xl '>
+					<span className='text-blue-400'>Espyon</span>{' '}
+					<span className='text-blue-600'>Ortograf</span> le{' '}
+					<span>correcteur</span> approprie
+				</h2>
+				<p className='mx-auto mt-12 max-w-xl text-lg text-center sm:text-gray-400  text-gray-500 leading-7'>
+					Augmenter votre productivite en corrigeant rapidement les fautes
+					d'ortographes de vos travaux ecrits : rapports, memoires , article...{' '}
+				</p>
+			</div>
 		</main>
 	);
 }
