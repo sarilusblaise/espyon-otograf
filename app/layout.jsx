83a3f3e5a290105a5ce32c8d1218bbd0f7a2ceac
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-07-11 22:11:47 -0400

    added new styles to home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className='bg-slate-950 text-white'>
-				<Header />
 				{children}
 				<Footer />
 			</body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import Header from '../components/Header';
+import Link from 'next/link';
 export default function HomePage() {
 	return (
-		<main>
-			<div>
-				<h2 className='mx-auto max-w-4xl font-display text-5xl text-center font-bold tracking-normal text-white sm:text-7xl '>
-					<span className='text-blue-400'>Espyon</span>{' '}
-					<span className='text-blue-600'>Ortograf</span> le{' '}
-					<span>correcteur</span> approprie
-				</h2>
-				<p className='mx-auto mt-12 max-w-xl text-lg text-center sm:text-gray-400  text-gray-500 leading-7'>
-					Augmenter votre productivite en corrigeant rapidement les fautes
-					d'ortographes de vos travaux ecrits : rapports, memoires , article...{' '}
-				</p>
-			</div>
-		</main>
+		<div className='flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen'>
+			<Header />
+			<main className='flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-10 mt-10'>
+				<div className='flex flex-col justify-center items-center'>
+					<h2 className='mx-auto max-w-4xl font-display text-3xl text-center font-bold tracking-normal text-white sm:text-7xl '>
+						<span className='text-blue-400'>Espyon</span>{' '}
+						<span className='text-blue-600'>Ortograf</span> le{' '}
+						<span>correcteur</span> approprie
+					</h2>
+					<p className='mx-auto mt-12 max-w-xl text-lg text-center sm:text-gray-400  text-gray-500 leading-7'>
+						Augmenter votre productivite en corrigeant rapidement les fautes
+						d'ortographes de vos travaux ecrits : rapports, memoires ,
+						article...{' '}
+					</p>
+					<Link
+						className='bg-emerald-700 flex-1 w-60 sm:w-80 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-emerald-600 transition'
+						href=''
+					>
+						sign up
+					</Link>
+				</div>
+			</main>
+		</div>
 	);
 }
