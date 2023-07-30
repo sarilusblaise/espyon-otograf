0c6ced4ee7ac8aed78610c3b0e7f7fefef365ccf
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-07-30 18:03:09 -0400

    created custom error file

--- app/icons/EyeOpen.jsx
+++ app/icons/EyeOpen.jsx
@@ -2,8 +2,8 @@ export default function EyeOpen({ className, title }) {
 	return (
 		<svg
 			width='22'
-			height='16'
-			viewBox='0 0 22 16'
+			height='17'
+			viewBox='0 0 22 17'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
 			className={className}
