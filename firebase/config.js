d733f2ff981197f871849ddb80dd11dcc7a59477
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-07-31 10:08:48 -0400

    added google client id

--- firebase/config.js
+++ firebase/config.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+	googleCLientId: process.env.NEXT_PUBLIC_FIREBASE_GOOGLE_CLIENT_ID,
 };
 
 // Initialize Firebase
